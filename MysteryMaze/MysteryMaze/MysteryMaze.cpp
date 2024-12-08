9e552e7fe5e01c08f58a07b1ec026c6bddf5154d
Javier Alvarez Sanzberro <jasanzberro@gmail.com>
2024-12-08 16:40:17 +0000

    2D array attempt 1
    TRIED SPAWINING THE BLOCKS in a 2D arrray

--- MysteryMaze/MysteryMaze/MysteryMaze.cpp
+++ MysteryMaze/MysteryMaze/MysteryMaze.cpp
@@ -1,10 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include <iostream> //for debugging
+#include <random>
+
+// declare the functions
+void generateMaze();
+
+sf::RenderWindow window(sf::VideoMode(1920, 1080), "MysteryMaze");// set the window size to my monitor size
 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "MysteryMaze");// set the window size to my monitor size
+
+	generateMaze();
 
     // get player texture as a Sprite and set the sprite's scale to 10x bigger and its origin to the center of the sprite 
     sf::Texture playerTexture;
@@ -21,6 +28,7 @@ int main()
     enemy.setScale(sf::Vector2f(10, 10));
     enemy.setPosition(sf::Vector2f(960, 540));
     enemy.setOrigin(sf::Vector2f(7, 7));
+
 
     sf::Clock clock; 
 
@@ -70,4 +78,43 @@ int main()
     }
 
     return 0;
+}
+
+void generateMaze()
+{
+	// generate a maze
+
+    sf::Texture wallTexture;
+    wallTexture.loadFromFile("Images/MysteryMaze_Wall_Bordered.png");
+    sf::Sprite wall(wallTexture);
+    wall.setScale(sf::Vector2f(10, 10));
+    wall.setPosition(sf::Vector2f(80, 80)); // add 80 at the beginning for the first wall to appear fully
+    wall.setOrigin(sf::Vector2f(8, 8));
+
+
+    sf::Vector2f currentWallPosition = wall.getPosition();
+
+    sf::Texture enemyTexture;
+    enemyTexture.loadFromFile("Images/MysteryMaze_Enemy.png");
+    sf::Sprite enemy(enemyTexture);
+    enemy.setScale(sf::Vector2f(10, 10));
+    enemy.setPosition(sf::Vector2f(960, 540));
+    enemy.setOrigin(sf::Vector2f(7, 7));
+
+
+	window.draw(wall);
+
+    int mazeArray[20][20];
+
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j < 20; j++)
+		{
+			mazeArray[i][j] = rand() % 2; // 0 or 1
+            sf::Vector2f offset(currentWallPosition.y * i, currentWallPosition.x * j); // the offset of the walls 
+
+			wall.setPosition(offset);
+			window.draw(wall); 
+		}
+	}
 }
